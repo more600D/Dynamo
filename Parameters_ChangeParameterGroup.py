10f46da8792b3a9faab41cfa82c3e8b276bc7ae4
more600D <s.s.sh@mail.ru>
2019-11-05 21:58:46 +0300

    изменение

--- Parameters_ChangeParameterGroup.py
+++ Parameters_ChangeParameterGroup.py
@@ -17,17 +17,16 @@ def GetParamByName(parameters, name):
             return p
 
 
+new_group = System.Enum.Parse(Autodesk.Revit.DB.BuiltInParameterGroup, IN[0])
 params = FilteredElementCollector(doc).OfClass(SharedParameterElement)
 param = GetParamByName(params, IN[1])
-params_definition = [i.GetDefinition() for i in params]
-new_group = System.Enum.Parse(Autodesk.Revit.DB.BuiltInParameterGroup, IN[0])
 
 TransactionManager.Instance.EnsureInTransaction(doc)
-if IN[2] is False:
-    for i in params_definition:
-        i.ParameterGroup = new_group
+if IN[2]:
+    for i in params:
+        i.GetDefinition().ParameterGroup = new_group
+        OUT = 'Изменение группирования всем параметрам'
 else:
     param.GetDefinition().ParameterGroup = new_group
+    OUT = 'Изменение группирования для ' + param.Name
 TransactionManager.Instance.TransactionTaskDone()
-
-OUT = params_definition, new_group, param

a617eb5d20928f5467ed73443eaddd413d1d7606
more600D <s.s.sh@mail.ru>
2019-11-06 18:49:52 +0300

    Изменение форматирования

--- Parameters_Replace_v2.py
+++ Parameters_Replace_v2.py
@@ -1,7 +1,4 @@
 import clr
-clr.AddReference("RevitNodes")
-import Revit
-clr.ImportExtensions(Revit.Elements)
 clr.AddReference("RevitServices")
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
@@ -14,8 +11,6 @@ app = uiapp.Application
 file = app.OpenSharedParameterFile()
 
 fp = doc.FamilyManager.get_Parameter(IN[0])
-
-SharFile = app.OpenSharedParameterFile()
 
 myExtDef = file.Groups.get_Item(IN[2]).Definitions.get_Item(IN[1]) 
 

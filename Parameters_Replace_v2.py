7978fae1c5d682436cb9f72048e3050e56491ee4
more600D <s.s.sh@mail.ru>
2019-11-06 18:43:38 +0300

    Удаление clr.AddReference("RevitAPI")

--- Parameters_Replace_v2.py
+++ Parameters_Replace_v2.py
@@ -5,13 +5,12 @@ clr.ImportExtensions(Revit.Elements)
 clr.AddReference("RevitServices")
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
-clr.AddReference("RevitAPI")
-from Autodesk.Revit.DB import FilteredElementCollector, FamilyManager
 
 
 doc = DocumentManager.Instance.CurrentDBDocument
 uiapp = DocumentManager.Instance.CurrentUIApplication
 app = uiapp.Application
+
 file = app.OpenSharedParameterFile()
 
 fp = doc.FamilyManager.get_Parameter(IN[0])

046dd0cb10d80a4e51318cf578459cc5925f8003
more600D <s.s.sh@mail.ru>
2019-11-25 19:00:23 +0300

    правки синтаксиса

--- Element_GetLocation.py
+++ Element_GetLocation.py
@@ -1,13 +1,14 @@
 import clr
 clr.AddReference('RevitAPI')
-from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, FamilyInstance, LocationPoint, Element, UnitUtils, DisplayUnitType
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, UnitUtils, DisplayUnitType
 clr.AddReference('RevitServices')
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
 doc = DocumentManager.Instance.CurrentDBDocument
 
-el = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_GenericModel).WhereElementIsNotElementType().ToElements()
+el = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_GenericModel) \
+    .WhereElementIsNotElementType().ToElements()
 
 el_filter = []
 point_z = []
@@ -15,15 +16,17 @@ point_z = []
 TransactionManager.Instance.EnsureInTransaction(doc)
 for i in el:
     param_group_model = i.Symbol.LookupParameter('Группа модели')
-    param_otmetka = i.LookupParameter(IN[1])
+    param_otmetka = i.LookupParameter(IN[1])  # noqa
     if param_group_model:
-        if param_group_model.AsString() == IN[0]:
+        if param_group_model.AsString() == IN[0]:  # noqa
             el_filter.append(i)
-            point = i.Location.Point
-            point_z.append(round(UnitUtils.ConvertFromInternalUnits(point.Z, DisplayUnitType.DUT_MILLIMETERS)))
+            pointXYZ = i.Location.Point
+            point_z.append(round(UnitUtils.ConvertFromInternalUnits(pointXYZ.Z, DisplayUnitType.DUT_MILLIMETERS)))
+    else:
+        OUT = "Нет совпадающих критериев"
     if param_otmetka:
         for p in point_z:
             param_otmetka.Set(UnitUtils.ConvertToInternalUnits(p, param_otmetka.DisplayUnitType))
+    else:
+        OUT = 'Нет такого параметра для записи значения'
 TransactionManager.Instance.TransactionTaskDone()
-
-OUT = 'Готово!'

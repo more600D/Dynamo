01d569c30415db11fbc3b02a64278bf61c398e95
more600D <s.s.sh@mail.ru>
2019-11-26 16:44:26 +0300

    исправление синтаксиса

--- FamilyParametres_AddParameters.py
+++ FamilyParametres_AddParameters.py
@@ -2,12 +2,10 @@ import clr
 import System
 
 clr.AddReference('RevitAPI')
-import Autodesk
-from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB import Transaction, BuiltInParameterGroup, ParameterType
 
 clr.AddReference('RevitServices')
 from RevitServices.Persistence import DocumentManager
-from RevitServices.Transactions import TransactionManager
 
 uiapp = DocumentManager.Instance.CurrentUIApplication
 doc = DocumentManager.Instance.CurrentDBDocument
@@ -18,23 +16,23 @@ fams = System.IO.Directory.GetFiles(mypath)
 fams2 = []
 
 # Flatten list
-params = [j for sub in IN[3] for j in sub] 
+params = [j for sub in IN[3] for j in sub]
 
 for i in fams:
-	if IN[1] == True: #Если True, то выбираются все файлы файлы в папке
+	if IN[1] is True:  # Если True, то выбираются все файлы файлы в папке
 		fams2.append(i)
-	elif IN[2] in i: #Если содержит значение в IN[2], то выбираются семейства удовлетворяющие значению
+	elif IN[2] in i:  # Если содержит значение в IN[2], то выбираются семейства удовлетворяющие значению
 		fams2.append(i)
 for doc in fams2:
-	familydoc = uiapp.Application.OpenDocumentFile(doc)#Открытие семейств
+	familydoc = uiapp.Application.OpenDocumentFile(doc)  # Открытие семейств
 	docs.append(familydoc)
 
 for f in docs:
-	t = Transaction(f, 'Добавление параметров') #Открытие транзакции
-	family_manager = f.FamilyManager #Получение FamilyManager
+	t = Transaction(f, 'Добавление параметров')  # Открытие транзакции
+	family_manager = f.FamilyManager  # Получение FamilyManager
 	t.Start()
 	for p in params:
-		family_manager.AddParameter(p, BuiltInParameterGroup.PG_TEXT, ParameterType.Text, True) #Добавление параметров
+		family_manager.AddParameter(p, BuiltInParameterGroup.PG_TEXT, ParameterType.Text, True)  # Добавление параметров
 	t.Commit()
 
 OUT = docs, params

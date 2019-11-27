e3de6b05b6df64140d3d5e6702a2be327185d834
more600D <s.s.sh@mail.ru>
2019-11-27 20:45:33 +0300

    создание нового варианта

---
+++ Element_GetLocation_v2.py
@@ -0,0 +1,36 @@
+import clr
+clr.AddReference("RevitAPI")
+from Autodesk.Revit.DB import FilteredElementCollector, RevitLinkInstance, BuiltInCategory
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import DocumentManager
+
+
+doc = DocumentManager.Instance.CurrentDBDocument
+
+
+def __flatten__(lists):
+    flat_list = []
+    for sublist in lists:
+        for item in sublist:
+            flat_list.append(item)
+    return flat_list
+
+
+rvt_links = FilteredElementCollector(doc).OfClass(RevitLinkInstance).ToElements()
+
+rvt_lights = []
+for r in rvt_links:
+    if "03-01_" in r.Name:
+        rvt_lights.append(r)
+
+ldoc = [r.GetLinkDocument() for r in rvt_lights]
+
+lights = []
+for l in ldoc:
+    item = []
+    el = FilteredElementCollector(l).OfCategory(BuiltInCategory.OST_LightingFixtures) \
+        .WhereElementIsNotElementType().ToElements()
+    item.append(el)
+    lights.append(item)
+
+OUT = __flatten__(lights)

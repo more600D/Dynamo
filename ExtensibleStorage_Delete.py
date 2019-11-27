6cedd5f92803b957b8a99e76d11cdca6024d9df4
more600D <s.s.sh@mail.ru>
2019-11-27 19:38:43 +0300

    Трубуется доработка

--- ExtensibleStorage_Delete.py
+++ ExtensibleStorage_Delete.py
@@ -4,6 +4,7 @@ from Autodesk.Revit.DB import FilteredElementCollector, ProjectInfo
 from Autodesk.Revit.DB.ExtensibleStorage import Schema
 clr.AddReference("RevitServices")
 from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
 
 doc = DocumentManager.Instance.CurrentDBDocument
 
@@ -21,4 +22,14 @@ item_guid = [i.GetEntitySchemaGuids() for i in info]
 
 sch = [Schema.Lookup(i) for i in __flatten__(item_guid)]
 
-OUT = sch
+TransactionManager.Instance.EnsureInTransaction(doc)
+
+for p in info:
+    for s in sch:
+        p.DeleteEntity(s)
+
+TransactionManager.Instance.TransactionTaskDone()
+
+sch_names = sch[0].SchemaName
+
+OUT = sch_names

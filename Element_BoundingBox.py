78563f2906f4198153d8beca7ec5319a8acd88db
more600D <s.s.sh@mail.ru>
2019-11-29 20:49:02 +0300

    коммит

---
+++ Element_BoundingBox.py
@@ -0,0 +1,28 @@
+import clr
+clr.AddReference("RevitAPI")
+from Autodesk.Revit.DB import UnitUtils, DisplayUnitType, Transaction
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI.Selection import ObjectType
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import DocumentManager
+
+
+doc = DocumentManager.Instance.CurrentDBDocument
+uidoc = DocumentManager.Instance.CurrentUIDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+
+t = Transaction(doc, 'Получаение бокса')
+t.Start()
+
+el_p = uidoc.Selection.PickObject(ObjectType.Element, 'Выбрать элемент')
+el = doc.GetElement(el_p)
+
+e = el.get_BoundingBox(doc.ActiveView)
+e_min = e.Min.X
+e_max = e.Max.X
+
+dis = UnitUtils.ConvertFromInternalUnits(e_max - e_min, DisplayUnitType.DUT_MILLIMETERS)
+t.Commit()
+
+OUT = el, dis

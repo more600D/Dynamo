d24cf3332b59dca9cf149f88485f3d7fa5a0614b
more600D <s.s.sh@mail.ru>
2020-01-28 13:26:56 +0300

    добавление разделения по типу вида

--- Views_unused.py
+++ Views_unused.py
@@ -1,6 +1,6 @@
 import clr
 clr.AddReference("RevitAPI")
-from Autodesk.Revit.DB import FilteredElementCollector, View3D, Viewport
+from Autodesk.Revit.DB import FilteredElementCollector, View, Viewport, ViewType
 clr.AddReference("RevitServices")
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
@@ -10,18 +10,27 @@ doc = DocumentManager.Instance.CurrentDBDocument
 uiapp = DocumentManager.Instance.CurrentUIApplication
 app = uiapp.Application
 
-views_col = FilteredElementCollector(doc).OfClass(View3D).ToElements()
+
+def not_null(el):
+    result = []
+    for i in el:
+        if i and not i.IsTemplate:
+            result.append(i)
+    return result
+
+
+views_col = FilteredElementCollector(doc).OfClass(View).ToElements()
 view_port = FilteredElementCollector(doc).OfClass(Viewport).ToElements()
 
 view_list = []
 view_3d_on = []
 
 for v in views_col:
-    if v and not v.IsTemplate:
+    if v.ViewType == ViewType.ThreeD and not v.IsTemplate:
         view_3d_on.append(v.Id)
 for p in view_port:
     view_list.append(p.ViewId)
 
 result = [doc.GetElement(r) for r in list(set(view_3d_on) - set(view_list))]
 
-OUT = result
+OUT = result, not_null(views_col)

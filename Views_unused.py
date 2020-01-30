8ce7245505487b4da85bd1bb47817744afa6d4a0
more600D <s.s.sh@mail.ru>
2020-01-30 15:10:39 +0300

    корректировка, добавление новых условий для видов

--- Views_unused.py
+++ Views_unused.py
@@ -1,6 +1,6 @@
 import clr
 clr.AddReference("RevitAPI")
-from Autodesk.Revit.DB import FilteredElementCollector, View, Viewport, ViewType
+from Autodesk.Revit.DB import FilteredElementCollector, View, Viewport, ViewType, BuiltInCategory
 clr.AddReference("RevitServices")
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
@@ -19,18 +19,49 @@ def not_null(el):
     return result
 
 
+def not_on_sheet(firstlist, secondlist):
+    return [doc.GetElement(r) for r in list(set(firstlist) - set(secondlist))]
+
+
 views_col = FilteredElementCollector(doc).OfClass(View).ToElements()
 view_port = FilteredElementCollector(doc).OfClass(Viewport).ToElements()
+schedule_graf = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_ScheduleGraphics)
 
 view_list = []
-view_3d_on = []
+schedule_list = []
+view_TreeD = []
+view_DraftingView = []
+view_Section = []
+view_FloorPlan = []
+view_Legend = []
+view_Schedule = []
 
 for v in views_col:
     if v.ViewType == ViewType.ThreeD and not v.IsTemplate:
-        view_3d_on.append(v.Id)
+        if '{' and '}' not in v.Name:
+            view_TreeD.append(v.Id)
+    if v.ViewType == ViewType.DraftingView and not v.IsTemplate:
+        if v.Name != "Стартовый лист":
+            view_DraftingView.append(v.Id)
+    if v.ViewType == ViewType.Section and not v.IsTemplate:
+        view_Section.append(v.Id)
+    if v.ViewType == ViewType.FloorPlan and not v.IsTemplate:
+        view_FloorPlan.append(v.Id)
+    if v.ViewType == ViewType.Legend and not v.IsTemplate:
+        view_Legend.append(v.Id)
+    if v.ViewType == ViewType.Schedule and not v.IsTemplate:
+        if '000_' not in v.Name:
+            view_Schedule.append(v.Id)
 for p in view_port:
     view_list.append(p.ViewId)
+for p in schedule_graf:
+    schedule_list.append(p.ScheduleId)
 
-result = [doc.GetElement(r) for r in list(set(view_3d_on) - set(view_list))]
+u_TreeD = not_on_sheet(view_TreeD, view_list)
+u_DraftingView = not_on_sheet(view_DraftingView, view_list)
+u_Section = not_on_sheet(view_Section, view_list)
+u_FloorPlan = not_on_sheet(view_FloorPlan, view_list)
+u_Legend = not_on_sheet(view_Legend, view_list)
+u_Schedule = not_on_sheet(view_Schedule, schedule_list)
 
-OUT = result, not_null(views_col)
+OUT = u_TreeD, u_DraftingView, u_Section, u_FloorPlan, u_Legend, u_Schedule, not_null(views_col)

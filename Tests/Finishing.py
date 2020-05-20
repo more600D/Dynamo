976a1b7f3a79733f35775cd807534f7af72a19df
more600D <s.s.sh@mail.ru>
2020-05-20 18:01:16 +0300

    отделка

--- Tests/Finishing.py
+++ Tests/Finishing.py
@@ -1,15 +1,39 @@
 import clr
 clr.AddReference("RevitAPI")
 from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, \
-    ElementCategoryFilter, LogicalOrFilter
+    ElementCategoryFilter, LogicalOrFilter, BuiltInParameter, UnitUtils
 clr.AddReference("RevitServices")
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
-
 doc = DocumentManager.Instance.CurrentDBDocument
 uiapp = DocumentManager.Instance.CurrentUIApplication
 app = uiapp.Application
+
+
+def getTypeOrInstanceParameter(elem, builtInParameter):
+    param = elem.get_Parameter(builtInParameter)
+    if param:
+        if param.AsDouble():
+            return param.AsDouble()
+    else:
+        if elem.Symbol.get_Parameter(builtInParameter).AsDouble():
+            return elem.Symbol.get_Parameter(builtInParameter).AsDouble()
+
+
+error = []
+
+
+def square(elem):
+    global error
+    values = []
+    try:
+        value = getTypeOrInstanceParameter(elem, BuiltInParameter.FURNITURE_WIDTH) * \
+            getTypeOrInstanceParameter(elem, BuiltInParameter.FAMILY_HEIGHT_PARAM)
+        values.append(value)
+    except:
+        error.append(elem)
+    return values
 
 
 door_cat = ElementCategoryFilter(BuiltInCategory.OST_Doors)
@@ -21,16 +45,27 @@ elements_col = FilteredElementCollector(doc).WherePasses(LogicalOrFilter(door_ca
 
 phase = doc.Phases[1]
 
-doors = []
-for room in room_col:
-    doors_in_room = []
-    for door in elements_col:
-        if door.FromRoom[phase]:
-            if door.FromRoom[phase].Id == room.Id:
-                doors_in_room.append(door)
-        if door.ToRoom[phase]:
-            if door.ToRoom[phase].Id == room.Id:
-                doors_in_room.append(door)
-    doors.append(doors_in_room)
+elements = []
+sum_square = []
 
-OUT = doors
+TransactionManager.Instance.EnsureInTransaction(doc)
+
+for room in room_col:
+    # param = room.LookupParameter('ПлощадьПроемов')
+    elements_in_room = []
+    elem_square = []
+    for elem in elements_col:
+        if elem.FromRoom[phase]:
+            if elem.FromRoom[phase].Id == room.Id:
+                elements_in_room.append(elem)
+                elem_square.append(square(elem))
+        if elem.ToRoom[phase]:
+            if elem.ToRoom[phase].Id == room.Id:
+                elements_in_room.append(elem)
+                elem_square.append(square(elem))
+    elements.append(elements_in_room)
+    sum_square.append(elem_square)
+
+TransactionManager.Instance.TransactionTaskDone()
+
+OUT = elements[0][0].get_Parameter(BuiltInParameter.FURNITURE_WIDTH)
---
+++ Tests/Finishing1.py
@@ -0,0 +1,64 @@
+import clr
+clr.AddReference("RevitAPI")
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, Outline, BoundingBoxIntersectsFilter, \
+    FamilyInstance, ElementCategoryFilter, BuiltInParameter, LogicalAndFilter, UnitUtils
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+
+def getTypeOrInstanceParameter(elem, builtInParameter):
+    param = elem.get_Parameter(builtInParameter)
+    if param:
+        value = param.AsDouble()
+        if value:
+            return value
+        else:
+            param = elem.Symbol.get_Parameter(builtInParameter)
+            return param.AsDouble()
+
+
+def square(elem):
+    value = getTypeOrInstanceParameter(elem, BuiltInParameter.FURNITURE_WIDTH) * \
+        getTypeOrInstanceParameter(elem, BuiltInParameter.FAMILY_HEIGHT_PARAM)
+    return value
+
+
+doc = DocumentManager.Instance.CurrentDBDocument
+
+door_cat = ElementCategoryFilter(BuiltInCategory.OST_Doors)
+win_cat = ElementCategoryFilter(BuiltInCategory.OST_Windows)
+
+room_col = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms).ToElements()
+
+elements = []
+square_value = []
+
+TransactionManager.Instance.EnsureInTransaction(doc)
+
+for room in room_col:
+    room_box = room.get_BoundingBox(None)
+    param = room.LookupParameter('ПлощадьПроемов')
+    if room_box:
+        outline = Outline(room_box.Min, room_box.Max)
+        bbfilter = BoundingBoxIntersectsFilter(outline)
+        doors = FilteredElementCollector(doc).OfClass(FamilyInstance). \
+            WherePasses(LogicalAndFilter(bbfilter, door_cat)).ToElements()
+        windows = FilteredElementCollector(doc).OfClass(FamilyInstance). \
+            WherePasses(LogicalAndFilter(bbfilter, win_cat)).ToElements()
+        fam_in_room = []
+        value = 0
+        for door in doors:
+            value += square(door)
+            fam_in_room.append(door)
+        for window in windows:
+            value += square(window)
+            fam_in_room.append(window)
+        param.Set(value)
+    elements.append(fam_in_room)
+    square_value.append(UnitUtils.ConvertFromInternalUnits(value, param.DisplayUnitType))
+
+
+TransactionManager.Instance.TransactionTaskDone()
+
+OUT = elements, square_value

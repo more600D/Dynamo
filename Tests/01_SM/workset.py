644a9c9ff47fcc0f083f75e713fe0a7e10d20e78
more600D <s.s.sh@mail.ru>
2020-06-26 19:25:03 +0300

    новые категории и проверки

--- Tests/01_SM/workset.py
+++ Tests/01_SM/workset.py
@@ -6,7 +6,6 @@ clr.AddReference("RevitServices")
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
-
 doc = DocumentManager.Instance.CurrentDBDocument
 
 
@@ -17,32 +16,85 @@ def GetWorkSet(name):
             return w.Id.IntegerValue
 
 
-def SetWorkSet(cat, name):
+def SetWorkSet_byCategory(cat, name):
     elems = FilteredElementCollector(doc).OfCategory(cat). \
         WhereElementIsNotElementType().ToElements()
 
     TransactionManager.Instance.EnsureInTransaction(doc)
-
-    for e in elems:
-        param = e.get_Parameter(BuiltInParameter.ELEM_PARTITION_PARAM)
-        if param:
-            worksetId = GetWorkSet(name)
-            if worksetId:
-                param.Set(worksetId)
-                msg = 'ОК'
-            else:
-                msg = 'Нет такого набора: {}'.format(name)
+    if elems:
+        for e in elems:
+            param = e.get_Parameter(BuiltInParameter.ELEM_PARTITION_PARAM)
+            if param:
+                worksetId = GetWorkSet(name)
+                if worksetId:
+                    try:
+                        param.Set(worksetId)
+                    except Exception:
+                        pass
+                    msg = 'ОК'
+                else:
+                    msg = 'Нет такого набора: {}'.format(name)
+    else:
+        msg = "Нет элементов категории {}".format(cat)
     return msg
 
     TransactionManager.Instance.TransactionTaskDone()
 
 
+def SetWorkSet_byElement(elem, name):
+    param = elem.get_Parameter(BuiltInParameter.ELEM_PARTITION_PARAM)
+    if param:
+        worksetId = GetWorkSet(name)
+        if worksetId:
+            param.Set(worksetId)
+            msg = 'ОК'
+        else:
+            msg = 'Нет такого набора: {}'.format(name)
+    return msg
+
+
+cats = []
+cats.append(BuiltInCategory.OST_Walls)
+cats.append(BuiltInCategory.OST_Floors)
+
 result = []
-result.append(SetWorkSet(BuiltInCategory.OST_Doors, '01_Двери'))
-result.append(SetWorkSet(BuiltInCategory.OST_Windows, '01_Окна'))
-result.append(SetWorkSet(BuiltInCategory.OST_StairsRailing, '02_Лестницы'))
-result.append(SetWorkSet(BuiltInCategory.OST_Stairs, '02_Лестницы'))
-result.append(SetWorkSet(BuiltInCategory.OST_Grids, '00_ОсиУровни'))
-result.append(SetWorkSet(BuiltInCategory.OST_Levels, '00_ОсиУровни'))
+
+TransactionManager.Instance.EnsureInTransaction(doc)
+
+for c in cats:
+    elems = FilteredElementCollector(doc).OfCategory(c). \
+        WhereElementIsNotElementType().ToElements()
+    for e in elems:
+        e_type = e.GetType().Name
+        elem_type = doc.GetElement(e.GetTypeId())
+        if e_type == "Wall":
+            param = e.get_Parameter(BuiltInParameter.WALL_STRUCTURAL_SIGNIFICANT)
+            param_func = elem_type.get_Parameter(BuiltInParameter.FUNCTION_PARAM)
+            if param and param.AsInteger() == 1:
+                result.append(SetWorkSet_byElement(e, '02_Стены'))
+            elif param.AsInteger() == 0 and param_func.AsValueString() == 'Наружные слои':
+                result.append(SetWorkSet_byElement(e, '01_Фасад'))
+            elif param.AsInteger() == 0 and param_func.AsValueString() == 'Внутренние слои':
+                result.append(SetWorkSet_byElement(e, '01_Перегородки'))
+        elif e_type == 'Floor':
+            param = e.get_Parameter(BuiltInParameter.FLOOR_PARAM_IS_STRUCTURAL)
+            param_func = elem_type.get_Parameter(BuiltInParameter.FUNCTION_PARAM)
+            if param and param.AsInteger() == 1:
+                result.append(SetWorkSet_byElement(e, '02_Перекрытия'))
+            elif param.AsInteger() == 0 and param_func.AsValueString() == 'Внутренние слои':
+                result.append(SetWorkSet_byElement(e, '01_АрхПол'))
+
+TransactionManager.Instance.TransactionTaskDone()
+
+result.append(SetWorkSet_byCategory(BuiltInCategory.OST_Doors, '01_Двери'))
+result.append(SetWorkSet_byCategory(BuiltInCategory.OST_Windows, '01_Окна'))
+result.append(SetWorkSet_byCategory(BuiltInCategory.OST_StairsRailing, '02_Лестницы'))
+result.append(SetWorkSet_byCategory(BuiltInCategory.OST_Stairs, '02_Лестницы'))
+result.append(SetWorkSet_byCategory(BuiltInCategory.OST_Grids, '00_ОсиУровни'))
+result.append(SetWorkSet_byCategory(BuiltInCategory.OST_Levels, '00_ОсиУровни'))
+result.append(SetWorkSet_byCategory(BuiltInCategory.OST_CLines, '00_ОпорныеПлоскости'))
+result.append(SetWorkSet_byCategory(BuiltInCategory.OST_Roofs, '01_Кровля'))
+result.append(SetWorkSet_byCategory(BuiltInCategory.OST_StructuralColumns, '02_Колонны'))
+result.append(SetWorkSet_byCategory(BuiltInCategory.OST_StructuralFraming, '02_Балки'))
 
 OUT = result

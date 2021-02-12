9e67c7468472203a481889fcdc2f478896cfeba8
more600D <s.s.sh@mail.ru>
2021-02-12 13:21:35 +0300

    check calculations

--- Room_calculator.py
+++ Room_calculator.py
@@ -38,18 +38,22 @@ def set_value_at_point(collector, param_name):
 def calculate_elements_in_room(room_collector, param_name):
     elements_in_rooms = []
     calculator = SpatialElementGeometryCalculator(doc)
+    bad_rooms = []
     for room in room_collector:
         room_number = room.get_Parameter(BuiltInParameter.ROOM_NUMBER).AsString()
         room_elements = []
-        cal_geometry = calculator.CalculateSpatialElementGeometry(room)
-        solid = cal_geometry.GetGeometry()
-        for face in solid.Faces:
-            for sub in cal_geometry.GetBoundaryFaceInfo(face):
-                el = doc.GetElement(sub.SpatialBoundaryElement.HostElementId)
-                set_value_to_parameter(el, param_name, room_number)
-                room_elements.append(el)
-        elements_in_rooms.append(room_elements)
-    return elements_in_rooms
+        try:
+            cal_geometry = calculator.CalculateSpatialElementGeometry(room)
+            solid = cal_geometry.GetGeometry()
+            for face in solid.Faces:
+                for sub in cal_geometry.GetBoundaryFaceInfo(face):
+                    el = doc.GetElement(sub.SpatialBoundaryElement.HostElementId)
+                    set_value_to_parameter(el, param_name, room_number)
+                    room_elements.append(el)
+            elements_in_rooms.append(room_elements)
+        except Exception:
+            bad_rooms.append(room)
+    return elements_in_rooms, bad_rooms
 
 
 col_room = FilteredElementCollector(doc).OfClass(SpatialElement).OfCategory(BuiltInCategory.OST_Rooms).ToElements()
@@ -58,4 +62,4 @@ col_roof = FilteredElementCollector(doc).OfClass(FootPrintRoof).ToElements()
 atpoint_rooms = set_value_at_point(col_roof, IN[1])  # noqa
 elements_in_rooms = calculate_elements_in_room(col_room, IN[1])  # noqa
 
-OUT = atpoint_rooms, elements_in_rooms
+OUT = atpoint_rooms, elements_in_rooms[0], elements_in_rooms[1]

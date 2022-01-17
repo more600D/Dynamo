18ae194418a6e49756fdf8cf14a003bb74044a37
more600d <s.s.sh@mail.ru>
2022-01-17 16:14:06 +0300

    mirred

--- Tests/mirred.py
+++ Tests/mirred.py
@@ -1,8 +1,26 @@
-elems = UnwrapElement(IN[0])  # noqa
+# -*- coding: utf-8 -*-
+import clr
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
 
+doc = DocumentManager.Instance.CurrentDBDocument
+
+elems = UnwrapElement(IN[1])  # noqa
+param_name = IN[2]  # noqa
+data = IN[3]  # noqa
 mirrored = []
-for el in elems:
-    if el.Mirrored:
-        mirrored.append(el)
 
-OUT = mirrored
+TransactionManager.Instance.EnsureInTransaction(doc)
+
+for el in elems:
+    param = el.LookupParameter(param_name)
+    if el.Mirrored and param:
+        mirrored.append(el)
+        param.Set(data)
+    elif param:
+        param.Set("")
+
+TransactionManager.Instance.TransactionTaskDone()
+
+OUT = len(elems)

3a3ae049442b14d3eb2c522101a08854f5ca23c9
more600D <sergio.jr.shvydko@gmail.com>
2022-08-09 14:28:24 +0200

    Opening

--- 08_UNIPRO/02_MakeTheOpening.py
+++ 08_UNIPRO/02_MakeTheOpening.py
@@ -18,32 +18,61 @@ def get_family_opening(family_name):
                   .ToElements()
     for fam in collector:
         fam_name = fam.Symbol.Family.Name
-        if fam_name == family_name:
+        if family_name in fam_name:
             result.append(fam)
     return result
 
 
-def cut_elements(fams):
+def set_time(fam_instance, mode=True, param_name='_Дата'):
+    res = ''
+    from datetime import datetime
+    dt_string = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+    param = fam_instance.LookupParameter(param_name)
+    if param:
+        if param.StorageType == DB.StorageType.String:
+            if mode:
+                if param.AsString() == '':
+                    param.Set(dt_string)
+            else:
+                param.Set(dt_string)
+    return dt_string
+
+
+def cut_filtered_elements(class_type, bb_filter, family):
     count = 0
+    elems_col = DB.FilteredElementCollector(doc)\
+                  .OfClass(class_type)\
+                  .WherePasses(bb_filter)\
+                  .ToElements()
+    if elems_col:
+        for f in elems_col:
+            try:
+                DB.InstanceVoidCutUtils.AddInstanceVoidCut(doc, f, family)
+                count += 1
+                set_time(family, False)
+            except: pass
+    return count
+
+
+def cut_elements(fams):
     TransactionManager.Instance.EnsureInTransaction(doc)
-    for fam in fams:
-        bbox = fam.get_BoundingBox(None)
-        bb_filter = DB.BoundingBoxIntersectsFilter(
-                       DB.Outline(bbox.Min, bbox.Max)
-                       )
-        floor_col = DB.FilteredElementCollector(doc)\
-                      .OfClass(DB.Floor)\
-                      .WherePasses(bb_filter)\
-                      .ToElements()
-        if floor_col:
-            for f in floor_col:
-                try:
-                    DB.InstanceVoidCutUtils.AddInstanceVoidCut(doc, f, fam)
-                    count += 1
-                except: pass
-    TransactionManager.Instance.TransactionTaskDone()
-    return 'Elements cutted - {}'.format(count)
+    floors = 0
+    walls = 0
+    if fams:
+        for fam in fams:
+            current_time = set_time(fam)
+            bbox = fam.get_BoundingBox(None)
+            bb_filter = DB.BoundingBoxIntersectsFilter(
+                        DB.Outline(bbox.Min, bbox.Max)
+                        )
+            floors += cut_filtered_elements(DB.Floor, bb_filter, fam)
+            walls += cut_filtered_elements(DB.Wall, bb_filter, fam)
+        TransactionManager.Instance.TransactionTaskDone()
+        result = 'Cutted elements - {}'.format(floors + walls)
+        data = result+'\n'+current_time
+        return data
+    else: return 'Family list is empty'
 
 
-name = 'UNI_Openning.Floor'
+name = IN[1]
 OUT = cut_elements(get_family_opening(name))

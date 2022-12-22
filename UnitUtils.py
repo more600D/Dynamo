d456fc321be476f59ad9c1bf7c1b45b1e980ac66
Sergei Shvydko <s.s.sh@mail.ru>
2022-12-21 19:24:01 -0500

    ForceTypeId

--- UnitUtils.py
+++ UnitUtils.py
@@ -3,21 +3,21 @@ clr.AddReference("RevitAPI")
 from Autodesk.Revit.DB import UnitUtils
 clr.AddReference("RevitServices")
 from RevitServices.Persistence import DocumentManager
-from RevitServices.Transactions import TransactionManager
 
 doc = DocumentManager.Instance.CurrentDBDocument
+
 
 def round_square(room_area, round_number=2):
     version = int(doc.Application.VersionNumber)
     if version > 2021:
         from Autodesk.Revit.DB import UnitTypeId
         dut = UnitTypeId.SquareMeters
-        value = round(UnitUtils.ConvertFromInternalUnits(room_area, dut),round_number)
+        value = round(UnitUtils.ConvertFromInternalUnits(room_area, dut), round_number)
         return UnitUtils.ConvertToInternalUnits(value, dut)
     else:
         from Autodesk.Revit.DB import DisplayUnitType
         dut = DisplayUnitType.DUT_SQUARE_METERS
-        value = round(UnitUtils.ConvertFromInternalUnits(room_area, dut),round_number)
+        value = round(UnitUtils.ConvertFromInternalUnits(room_area, dut), round_number)
         return UnitUtils.ConvertToInternalUnits(value, dut)
 
 room = UnwrapElement((IN[1]))  # noqa

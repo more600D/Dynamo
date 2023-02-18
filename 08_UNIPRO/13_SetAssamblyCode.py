9f40a41439b11a1686a902bb6cc3ca052cca9b63
more600d <s.s.sh@mail.ru>
2023-02-18 14:32:32 +0100

    AssemblyCode

---
+++ 08_UNIPRO/13_SetAssamblyCode.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import clr
+clr.AddReference("RevitAPI")
+from Autodesk.Revit.DB import FilteredElementCollector, WallType, FloorType, CeilingType
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+doc = DocumentManager.Instance.CurrentDBDocument
+data = IN[0]
+sheet_name = IN[1]
+
+
+def get_all_types(document, elemType):
+    return FilteredElementCollector(document).OfClass(elemType).ToElements()
+
+
+def set_assembly_code(type_list, data):
+    from Autodesk.Revit.DB import BuiltInParameter
+    info = ":("
+    result = []
+    for elem in type_list:
+        name = elem.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_NAME).AsString()
+        assembly_param = elem.get_Parameter(BuiltInParameter.UNIFORMAT_CODE)
+        for key in data.keys():
+            if name == key:
+                value = data[key]
+                if value == 999:
+                    assembly_param.Set("999")
+                else:
+                    assembly_param.Set(value)
+                info = ":)"
+        result.append("{} - {}".format(name, info))
+    return result
+
+
+TransactionManager.Instance.EnsureInTransaction(doc)
+if sheet_name == "Стены":
+    result = set_assembly_code(get_all_types(doc, WallType), data)
+elif sheet_name == "Перекрытия":
+    result = set_assembly_code(get_all_types(doc, FloorType), data)
+elif sheet_name == "Потолок":
+    result = set_assembly_code(get_all_types(doc, CeilingType), data)
+TransactionManager.Instance.TransactionTaskDone()
+
+OUT = result

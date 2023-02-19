4ef92b80b598fcad7046f039cd83bc9b3c217168
more600d <s.s.sh@mail.ru>
2023-02-19 11:32:46 +0100

    date

--- 06_not_revit/write_txt.py
+++ 06_not_revit/write_txt.py
@@ -1,11 +1,13 @@
 import os
 import clr
+from datetime import datetime
 from io import open
 data = IN[0]
 clr.AddReference("RevitServices")
 from RevitServices.Persistence import DocumentManager
 doc = DocumentManager.Instance.CurrentDBDocument
-homedir = os.path.expanduser('~') + r'\\Desktop\\{}_{}.txt'.format(doc.Title, IN[1])
+date = datetime.now()
+homedir = os.path.expanduser('~') + r'\\Desktop\\{}_{}_{}.txt'.format(doc.Title, IN[1], date.strftime("%Y%m%d%I%M%S"))
 with open(homedir, 'w', newline='') as file:
     for d in data:
         file.write(d + '\n')
